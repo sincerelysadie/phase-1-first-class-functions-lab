54e1fe06c47479dfb9eabf049ae6ab1237e5aaea
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-04-05 21:02:58 -0700

    commit tree ab09b9333f40b6cda54a137537a3ce8efad33a33

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4);
+}
+function selectingDrivers() {
+    return (returnFirstTwoDrivers, returnLastTwoDrivers);
+}
+
